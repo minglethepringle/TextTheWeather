47461b9fc703d74e90568b8e02e45b2919d1a9ed
Mingle Li <limingle5@gmail.com>
2024-04-01 23:12:02 -0400

    Git ignore

--- TextTheWeather.Core/Program.cs
+++ TextTheWeather.Core/Program.cs
@@ -13,13 +13,27 @@ namespace TextTheWeather.Core
     {
         static async Task Main(string[] args)
         {
-            bool test = true;
-            User recipient = new()
+            List<User> recipients = new List<User>();
+            recipients.Add(new()
             {
-                FirstName = test ? "Mingle" : "Lilli",
-                LastName = test ? "Li" : "Tobe",
-                Email = test ? "limingle5@gmail.com" : "lilli.tobe@gmail.com",
-                PhoneNumber = test ? "7746153717" : "7742906766",
+                FirstName = "Mingle",
+                LastName = "Li",
+                Email = "limingle5@gmail.com",
+                PhoneNumber = "7746153717",
+                Latitude = "38.963421335750915",
+                Longitude = "-77.08732715397758",
+                WeatherFrom = new TimeOnly(7, 0),
+                WeatherTo = new TimeOnly(20, 0),
+                TextWeather = true,
+                EmailWeather = true,
+                IsPremium = true
+            });
+            recipients.Add(new()
+            {
+                FirstName = "Lilli",
+                LastName = "Tobe",
+                Email = "lilli.tobe@gmail.com",
+                PhoneNumber = "7742906766",
                 Latitude = "38.90072056045205",
                 Longitude = "-77.0497969457071",
                 WeatherFrom = new TimeOnly(7, 0),
@@ -27,23 +41,40 @@ namespace TextTheWeather.Core
                 TextWeather = true,
                 EmailWeather = true,
                 IsPremium = true
-            };
+            });
+            recipients.Add(new()
+            {
+                FirstName = "Zach",
+                LastName = "Simon",
+                Email = "zach.simon027@gmail.com",
+                PhoneNumber = "7202336158",
+                Latitude = "38.904169160076826",
+                Longitude = "-77.02937033057403",
+                WeatherFrom = new TimeOnly(7, 0),
+                WeatherTo = new TimeOnly(20, 0),
+                TextWeather = true,
+                EmailWeather = true,
+                IsPremium = true
+            });
             
-            // Get weather from OpenWeather API
-            IWeatherApi weatherApi = new OpenWeatherApi();
-            OpenWeatherApiResponse weather = weatherApi.GetWeather(recipient.Latitude, recipient.Longitude).Result;
+            foreach (User recipient in recipients)
+            {
+                // Get weather from OpenWeather API
+                IWeatherApi weatherApi = new OpenWeatherApi();
+                OpenWeatherApiResponse weather = weatherApi.GetWeather(recipient.Latitude, recipient.Longitude).Result;
             
-            // Map
-            IMapper<OpenWeatherApiResponse, string> mapper = new WeatherToHumanMapper(recipient);
-            string humanReadableWeather = mapper.Map(weather);
-            Console.WriteLine(humanReadableWeather);
+                // Map
+                IMapper<OpenWeatherApiResponse, string> mapper = new WeatherToHumanMapper(recipient);
+                string humanReadableWeather = mapper.Map(weather);
+                Console.WriteLine(humanReadableWeather);
             
-            // Send weather to Twilio API
-            IWeatherSender sender = new SendGridApi();
+                // Send weather to Twilio API
+                IWeatherSender sender = new SendGridApi();
             
-            // ITwilioApi twilioApi = new TwilioApi();
-            // twilioApi.SendSms("8777804236", humanReadableWeather);
-            await sender.SendWeather(recipient, humanReadableWeather);
+                // ITwilioApi twilioApi = new TwilioApi();
+                // twilioApi.SendSms("8777804236", humanReadableWeather);
+                await sender.SendWeather(recipient, humanReadableWeather);
+            }
         }
     }
 }
